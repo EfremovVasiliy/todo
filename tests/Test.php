a0ad35bf9e31cfbf3210dc63ad2ab9c9b52f8b25
Vasiliy Efremov <efremov.vasiliy00@gmail.com>
2022-10-16 15:22:40 +0600

    Started todo project

---
+++ tests/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    public function test_one(): void
+    {
+        $this->assertTrue(true);
+    }
+}
